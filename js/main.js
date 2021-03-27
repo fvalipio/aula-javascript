587b0d8d5f55065bf81437dd39a6ec701a84ddc9
Francilaine Alipio <fvalipio@gmail.com>
2021-03-27 19:11:33 -0300

    introducao js

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+/*
+var d = new Date();
+alert(d);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count = count + 1; //tambem pode usar count ++
+}*/
+
+/*
+var idade = prompt("Qual sua idade?")
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else {
+    alert("menor de idade");
+};
+*/
+
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]; //transformando em lista
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã", "pêra", "laranja"];
+//console.log(lista)
+//console.log(lista[1]); //exibe item 1
+//lista.push("uva"); //incluir item na lista
+//lista.pop(); //remover item da lista
+//console.log(lista.length); //quantidade de itens na lista
+//console.log(lista.reverse()); //altera ordem dos itens na lista
+//console.log(lista.toString()); //transforma em string
+//console.log(lista.toString()[0]) //exibe primeira letra item 0
+//console.log(lista.join(" - ")); //caractere de junção dos itens da lista
+
+
+//var nome = "Francilaine";
+//var idade = 34;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
