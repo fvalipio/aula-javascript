0d77a0f7e4dee0805dd6fde5032d36a9214e081a
Francilaine Alipio <fvalipio@gmail.com>
2021-03-29 22:24:33 -0300

    parte final aula

--- js/main.js
+++ js/main.js
@@ -1,3 +1,57 @@
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"))
+
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar() {
+    window.open("https://www.linkedin.com/in/francilaine-viana/");
+    //window.location.href = "https://www.linkedin.com/in/francilaine-viana/";
+}
+
+function trocar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto")
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load() {
+    alert("Página carregada")
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value)
+}
+
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+}
+alert(soma(5, 10));
+*/
+
+/*
+var validar = 0;
+function validaIdade(idade) {
+    if (idade >= 18) {
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt ("Qual sua idade?");
+validaIdade(idade)
+console.log(validar);
+*/
+
 
 /*
 var d = new Date();
